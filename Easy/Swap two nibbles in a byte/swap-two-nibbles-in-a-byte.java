0b0b69dc276d6e88bb4802a29ca685d14b7d75c6
Jahnvi bansal <94457646+Jahnvi-bansal@users.noreply.github.com>
2024-05-31 22:20:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Swap two nibbles in a byte/swap-two-nibbles-in-a-byte.java
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.swapNibbles(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int swapNibbles(int x) {
+        // code here
+        int a = x % 16; 
+        int b = x / 16; 
+        int ans = a * 16 + b; 
+        return ans; 
+
+    }
+}
